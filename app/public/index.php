b8111e84a8c42515dd72452bc5f26f67371a5e9e
Eduardo PINA <e.pina@adlperformance.fr>
2023-04-14 09:40:08 +0200

    add some config and symfony-dumper

--- app/public/index.php
+++ app/public/index.php
@@ -1,4 +1,13 @@
 <?php
 declare(strict_types=1);
 
+require_once dirname(__DIR__) . '/vendor/autoload.php';
+
+// request received
+// dd('here');
+
+// perform some log
+
+// send response (string of content)
+
 echo 'hello eduardo!!!!';
